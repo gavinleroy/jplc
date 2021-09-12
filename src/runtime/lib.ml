7ebe91f7e5afbd281665c47b14c1b63e6f7ceda9
Gavin Gray <gavinleroy6@gmail.com>
2021-09-12 21:24:34 +0200

    outlined runtime lib

--- src/runtime/lib.ml
+++ src/runtime/lib.ml
@@ -11,3 +11,29 @@ let show =
 
 let print =
   foreign "print" (string @-> returning void)
+
+(* TODO make the following work *)
+(* struct pict {
+ *     int rows;
+ *     int cols;
+ *     double *data;
+ * }; *)
+(* let read_image =
+ *   foreign "read_image" (string @-> returning pict) *)
+(* let write_image =
+ *   foreign "write_image" (pict @-> string @-> returning void) *)
+(* let has_size =
+ *   foreign "has_size" (pict @-> int32_t @-> int32_t @-> returning int32_t) *)
+(* int32_t has_size(struct pict input, int64_t rows, int64_t cols); *)
+(* let sepia =
+ *   foreign "sepia" (pict @-> returning pict)
+ * struct pict sepia(struct pict input); *)
+(* let blur =
+ *   foreign "blur" (pict @-> double @-> returning pict)
+ *   struct pict blur(struct pict input, double radius); *)
+(* let resize =
+ *   foreign "resize" (pict @-> int32_t @-> int32_t @-> returning pict)
+ *     struct pict resize(struct pict input, int64_t rows, int64_t cols); *)
+(* let crop =
+ *   foreign "crop" (pict @-> int32_t @-> @-> int32_t @-> int32_t @-> int32_t @-> returning pict)
+ *   struct pict crop(struct pict input, int64_t top, int64_t left, int64_t bottom, int64_t right); *)
